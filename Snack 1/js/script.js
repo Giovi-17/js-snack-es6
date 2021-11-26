36ba039bafaf94b2e9bd9f183646bf64cb1a7183
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-11-26 15:45:42 +0100

    Snack 1

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -4,3 +4,77 @@ con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore 
 utilizzando destructuring e template literal nel DOM */
 
+let bici = [
+
+    {
+    
+        nome: "Canyon Ultimate",
+        peso: 7
+
+    },
+
+    {
+    
+        nome: "Rose X-Lite",
+        peso: 8
+
+    },
+
+    {
+    
+        nome: "Sarto asola",
+        peso: 6
+
+    },
+
+    {
+    
+        nome: "Colnago V2-R ",
+        peso: 1
+
+    },
+
+    {
+    
+        nome: "Scott Addict RC",
+        peso: 4
+
+    },
+
+    {
+    
+        nome: "Trek Èmonda",
+        peso: 9
+
+    }
+
+]
+
+
+//ciclo for che stabilisce il peso minore fra le bici da corsa
+//se il valore del peso dell oggetto che scorre è minore 
+//di quello di default allora sostituisce il valore di default
+
+//pesoMinBici --> oggetto bici di default (il primo oggetto bici)
+let pesoMinBici = bici[0];
+
+//thisBici --> oggetto bici che scorre in base al contatore
+for( let i = 0; i < bici.length; i++ ){
+
+    const thisBici = bici[i];
+
+    if( thisBici.peso < pesoMinBici.peso ){
+
+        pesoMinBici = thisBici;
+
+    }
+
+}
+
+const biciLeggera = document.getElementById("bici-leggera");
+biciLeggera.innerHTML= `
+
+    La bici più leggera è <span>${pesoMinBici.nome}</span> e pesa <span>${pesoMinBici.peso}kg</span>
+
+`;
+
